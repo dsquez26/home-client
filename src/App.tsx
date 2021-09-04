4bf30de64f1c0d3614eaec5bc8fe280dbdf58d7e
David Lee Vazquez <davidvazquez1026@gmail.com>
2021-09-04 15:43:14 -0500

    first call to service

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Login from './Components/Login/Login';
 import Header from './Components/Header/Header';
+import ApiFetch from './api/api';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Login></Login>
+      <ApiFetch />
     </div>
 
   );
