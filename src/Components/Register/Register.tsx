e285de002c45bd8383f26e16f76c1fd6e4b0e356
David Vazquez <davidvazquez1026@gmail.com>
2021-09-06 11:29:12 -0500

    added basic error msging for the forms

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -2,8 +2,7 @@ import { useForm } from 'react-hook-form'
 // import { useState, useEffect } from 'react'
 
 import './Register.css'
-
-interface RegisterFormData  {
+interface RegisterFormData {
     firstName: String;
     lastName: String;
     email: String;
@@ -11,24 +10,28 @@ interface RegisterFormData  {
     employeeID: Number
 }
 
-
 const Register = () => {
-    const { register, handleSubmit } = useForm<RegisterFormData>();
+    const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
 
-    const formSubmitted = handleSubmit(data => console.log('data!', data)) 
+    const formSubmitted = handleSubmit(data => console.log('data!', data))
 
     return (
         <div className='register-form-ctn'>
             <p>
                 New? Register Here!
             </p>
-            <form className='register-form' onSubmit={ formSubmitted }>
-                <input type='input' placeholder='First Name'{...register ("firstName", {required: true, min: 2, maxLength: 16})} />
-                <input type='input' placeholder='Last Name'{...register ("lastName", {required: true, min: 2, maxLength: 16})} />
-                <input type='input' placeholder='Email'{...register ("email", {required: true, min: 2, maxLength: 16})} />
-                <input type='input' placeholder='Phone Number'{...register ("phoneNumber", {required: true, min: 2, maxLength: 16})} />
-                <input type='input' placeholder='Employee ID'{...register ("employeeID", {required: true, min: 2, maxLength: 16})} />
+            <form className='register-form' onSubmit={formSubmitted}>
+                <input type='input' placeholder='First Name'{...register("firstName", { required: true, min: 6, maxLength: 16 })} />
+                {errors.firstName && "First name is required"}
+                <input type='input' placeholder='Last Name'{...register("lastName", { required: true, min: 2, maxLength: 16 })} />
+                {errors.lastName && "Last name is required"}
+                <input type='input' placeholder='Email'{...register("email", { required: true, min: 2, maxLength: 16 })} />
+                {errors.email && "Email is required"}
+                <input type='input' placeholder='Phone Number'{...register("phoneNumber", { required: true, min: 2, maxLength: 16 })} />
+                {errors.phoneNumber && "Phone Number is required"}
+                <input type='input' placeholder='Employee ID'{...register("employeeID", { required: true, min: 2, maxLength: 16 })} />
+                {errors.employeeID && "Employee ID is required"}
                 <input type='submit'></input>
             </form>
         </div>
